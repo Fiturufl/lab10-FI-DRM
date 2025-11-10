c654c6022b3ca4282c83a33b262b4a39c8904b4b
Francisco Iturriaga <franciscoiturriaga@Franciscos-MacBook-Pro.local>
2025-11-10 14:31:08 -0400

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -4,8 +4,32 @@ calculator.py
 
 One function per operation, in order.
 """
-# First example
-def add(a, b): 
-    pass
+# https://github.com/Fiturufl/lab10-FI-DRM
+# Partner 1: Francisco Iturriaga
+# Partner 2: David Reyes-Munoz
 
+import math
 
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if b == 0:
+        raise ZeroDivisionError("Cannot divide by zero")
+    return a / b
+
+def log(a, b):
+    if a <= 0:
+        raise ValueError("Input must be positive")
+    if b <= 0 or b == 1:
+        raise ValueError("Base must be positive and not equal to 1")
+    return math.log(a, b)
+
+def exp(a, b):
+    return a ** b
